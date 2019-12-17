0624c1a4e0f259325f894d0fba0060cc25ad4b59
KeimaShikai <shikamaru740@gmail.com>
2019-12-17 22:52:08 +0700

    Added writing into the file

--- oca.py
+++ oca.py
@@ -6,6 +6,7 @@
 
 from requests import get
 from re import findall
+from time import strftime
 
 
 '''FUTURE DOCS INFO
@@ -24,8 +25,13 @@ REGULAR = r'>([а-яА-Я ]*)<\/a><\/td><td class="[\w\s-]*"><span id="[\w_-]*" 
            '"><span id="[\w_-]*" class="[\w_-]*" data-curse-val="([\d.]*)" data'
 
 
-currency_values = {'Доллар':'usd', 'Евро':'eur', 'Фунт':'gpb', 'Тенге':'kzt',
-                   'Юань':'cny', 'Франк':'chf', 'Йена':'jpy'}
+currency_values = {'Доллар' : 'usd',
+                   'Евро'   : 'eur',
+                   'Фунт'   : 'gpb',
+                   'Тенге'  : 'kzt',
+                   'Юань'   : 'cny',
+                   'Франк'  : 'chf',
+                   'Йена'   : 'jpy'}
 
 # city_values = ('moskva', 'sankt-peterburg', 'ekaterinburg', 'kazan',
                # 'nizhniy-novgorod', 'novosibirsk', 'omsk', 'samara',
@@ -36,22 +42,37 @@ currency_values = {'Доллар':'usd', 'Евро':'eur', 'Фунт':'gpb', 'Т
                # 'yaroslavl', 'vladivostok', 'mahachkala', 'tomsk',
                # 'orenburg', 'kemerovo', 'novokuzneck')
 
-city_values = {'Москва':'moskva', 'Санкт-Петербург':'sankt-peterburg',
-               'Екатеринбург':'ekaterinburg', 'Kазань':'kazan',
-               'Нижний Новгород':'nizhniy-novgorod', 'Новосибирск':'novosibirsk',
-               'Омск':'omsk', 'Самара':'samara',
-               'Челябинск':'chelyabinsk', 'Ростов-на-Дону':'rostov-na-donu',
-               'Уфа':'ufa', 'Красноярск':'krasnoyarsk',
-               'Пермь':'perm', 'Воронеж':'voronezh',
-               'Волгоград':'volgograd', 'Краснодар':'krasnodar',
-               'Саратов':'saratov', 'Тюмень':'tumen',
-               'Тольятти':'tolyatti', 'Ижевск':'izhevsk',
-               'Барнаул':'barnaul', 'Иркутск':'irkutsk',
-               'Ульяновск':'ulyanovsk', 'Хабаровск':'habarovsk',
-               'Ярославль':'yaroslavl', 'Владивосток':'vladivostok',
-               'Махачкала':'mahachkala', 'Томск':'tomsk',
-               'Оренбург':'orenburg', 'Кемерово':'kemerovo',
-               'Новокузнецк':'novokuzneck'}
+city_values = {'Москва'          : 'moskva',
+               'Санкт-Петербург' : 'sankt-peterburg',
+               'Екатеринбург'    : 'ekaterinburg',
+               'Kазань'          : 'kazan',
+               'Нижний Новгород' : 'nizhniy-novgorod',
+               'Новосибирск'     : 'novosibirsk',
+               'Омск'            : 'omsk',
+               'Самара'          : 'samara',
+               'Челябинск'       : 'chelyabinsk',
+               'Ростов-на-Дону'  : 'rostov-na-donu',
+               'Уфа'             : 'ufa',
+               'Красноярск'      : 'krasnoyarsk',
+               'Пермь'           : 'perm',
+               'Воронеж'         : 'voronezh',
+               'Волгоград'       : 'volgograd',
+               'Краснодар'       : 'krasnodar',
+               'Саратов'         : 'saratov',
+               'Тюмень'          : 'tumen',
+               'Тольятти'        : 'tolyatti',
+               'Ижевск'          : 'izhevsk',
+               'Барнаул'         : 'barnaul',
+               'Иркутск'         : 'irkutsk',
+               'Ульяновск'       : 'ulyanovsk',
+               'Хабаровск'       : 'habarovsk',
+               'Ярославль'       : 'yaroslavl',
+               'Владивосток'     : 'vladivostok',
+               'Махачкала'       : 'mahachkala',
+               'Томск'           : 'tomsk',
+               'Оренбург'        : 'orenburg',
+               'Кемерово'        : 'kemerovo',
+               'Новокузнецк'     : 'novokuzneck'}
 
 
 def main():
@@ -64,7 +85,7 @@ def main():
     while (True):
         switch = input('Введите номер действия: ')
         if   (switch == "1"):
-            process_data(URL_BASE + 'usd/tomsk')
+            process_data()
             quit()
         elif (switch == "2"):
             filtered_search()
@@ -83,11 +104,13 @@ def show_help_info():
     print('Данная софтина предоставляет возможность за несколько секунд\n'
           'получить информации о курсе определенной валюты в различныйх\n'
           'банках через вашу консоль. Быстро и удобно.\n')
-    print('Имеется возможность вывода информации в консоль или записи\n'
-          'в файл.')
-    print('\nВ качестве валют Вы можете выбрать следующие варианты:')
+    print('Имеется возможность вывода информации в консоль\n'
+          'или записи в файл.')
+    print()
+    print('В качестве валют Вы можете выбрать следующие варианты:')
     print(', '.join(currency_values))
-    print('\nВ качестве городов Вы можете выбрать следующие варианты:')
+    print()
+    print('В качестве городов Вы можете выбрать следующие варианты:')
     city_list = list(city_values)
     for i in range(0, len(city_list), 3):
         print(', '.join(city_list[i:i + 3]))
@@ -104,30 +127,42 @@ def filtered_search():
         print('Вы ввели неверное название города!')
         quit()
 
-    process_data(URL_BASE + currency_values.get(currency) +
-                      '/' + city_values.get(city))
+    process_data(currency, city)
 
-def process_data(url):
+def process_data(currency="Доллар", city="Томск"):
     """some info"""
-    content = get(url).content.decode('utf-8')
+    content = get(URL_BASE + currency_values.get(currency)
+                     + '/' + city_values.get(city)).content.decode('utf-8')
     data = findall(REGULAR, content)
+    print()
+    print('Сводка на данный момент времени:\n')
     for each in data:
-        print("{0}: \nпокупка: {1}, продажа: {2}\n".format(each[0],
-                                                           each[1],
-                                                           each[2]))
+        print("{0}:\nпокупка: {1} руб, продажа: {2} руб\n".format(each[0],
+                                                                  each[1],
+                                                                  each[2]))
+    print()
+    if (input('Введите \'1\' для записи в файл: ') == '1'):
+        file_name = currency + '_' + city + strftime('_%d_%b_%H:%M.log')
+        with open(file_name, 'w') as file:
+            for each in data:
+                file.write("{0}:\nпокупка: {1} руб, "\
+                           "продажа: {2} руб\n".format(each[0],
+                                                       each[1],
+                                                       each[2]))
+
 
 # TODO remove this function
 def test():
     with open('tomsk.html', 'r') as test:
         t = test.read()
         data = findall(REGULAR, t)
+        print('Сводка на данный момент времени:\n')
         for i in data:
-            print("{0}: \nпокупка: {1}, продажа: {2}\n".format(i[0], i[1], i[2]))
+            print("{0}: \nпокупка: {1} руб, продажа: {2} руб\n".format(i[0], i[1], i[2]))
 
 
-# TODO add writing into the file
 # TODO add output dividers through the decorators
-# TODO test all parts
+# TODO test all parts (and check names
 # TODO comment the code
 # TODO fix README
 # TODO add GUI?
